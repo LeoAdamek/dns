dad1e0f48a58bffdc75aa8891811c0646fabd28c
Leo Adamek <leo.adamek@mrzen.com>
2017-05-25 09:44:06 +0100

    Added megumi to the dns

--- templates.lua
+++ templates.lua
@@ -2,4 +2,6 @@ function breaker_of_things(domain)
     a(domain, "139.162.242.110")
     aaaa(domain, "2a01:7e00::f03c:91ff:fed5:6a9e")
     alias(concat("www", domain), domain)
+    a("megumi", "139.162.242.110")
+    aaaa("megumi6", "2a01:7e00::f03c:91ff:fed5:6a9e")
 end
