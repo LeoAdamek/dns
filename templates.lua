199091332cf52cc82330bb372f8916f0abb1ef92
Leo Adamek <leo@adamek.me>
2017-05-25 21:22:59 +0100

    Added SPF

--- templates.lua
+++ templates.lua
@@ -6,4 +6,5 @@ function breaker_of_things(domain)
     aaaa("megumi6", "2a01:7e00::f03c:91ff:fed5:6a9e")
     
     txt("megumi._domainkey", "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9bwJJXjMLUn2JbC4OBMWX5giuhxcGbdtrSEs0+2SWLOe6BDS+xVwsT1StWjBUq/2liJicFoLuxhQFNn5hxzxVKiaPQ3eZc/TNy79nD/m8hqzqCmC6ZAtdg6wYkk6VCMYpHzZ7PgwJB6bE96ci072HaG/YNqKe2xEbM95ryjNo+wIDAQAB")
+    txt(domain, "v=spf1 a:megumi.breakerofthings.email a:megumi6.breakerofthings.email -all")
 end
