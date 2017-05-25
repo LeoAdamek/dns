e1bf08bdd82ba00919ca82bf90a47852fd5166d4
Leo Adamek <leo@adamek.me>
2017-05-25 20:48:02 +0100

    Added DKIM Record

--- templates.lua
+++ templates.lua
@@ -4,4 +4,6 @@ function breaker_of_things(domain)
     alias(concat("www", domain), domain)
     a("megumi", "139.162.242.110")
     aaaa("megumi6", "2a01:7e00::f03c:91ff:fed5:6a9e")
+    
+    txt(domain, "v=DKIM1; k=rsa; p=p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9bwJJXjMLUn2JbC4OBMWX5giuhxcGbdtrSEs0+2SWLOe6BDS+xVwsT1StWjBUq/2liJicFoLuxhQFNn5hxzxVKiaPQ3eZc/TNy79nD/m8hqzqCmC6ZAtdg6wYkk6VCMYpHzZ7PgwJB6bE96ci072HaG/YNqKe2xEbM95ryjNo+wIDAQAB")
 end
