6a9f74a9033f9213623a0ed909afdbbd3ff8aba4
Leo Adamek <leo@adamek.me>
2017-05-25 21:49:20 +0100

    Added explicit ip6 to SPF

--- templates.lua
+++ templates.lua
@@ -8,7 +8,7 @@ function breaker_of_things(domain)
     dkim_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGnojLHjH8AQXRmv63TlYapXXP+TS6no9Meljx8V3RqXWDwqjHTES5UgFdPk5qftHeKOK79/ZzMq5gZrrvN97FOAWb4q+tEKZzm3eKyN4A0w/vs/aLy3gr4JsV1skfrxKmXdtxDDimq7yKGunnBvm52yO2OyUHRdzuBspSU/jQ1QIDAQAB"
     
     txt("megumi._domainkey", "v=DKIM1; k=rsa; p="..dkim_key)
-    txt(domain, "v=spf1 a:megumi.breakerofthings.email a:megumi6.breakerofthings.email -all")
+    txt(domain, "v=spf1 a:megumi.breakerofthings.email ip6:2a01:7e00:0:0:f03c:91ff:fed5:6a9e -all")
     
     mx(domain, "megumi.breakerofthings.email", 1)
     mx(domain, "megumi6.breakerofthings.email", 2)
