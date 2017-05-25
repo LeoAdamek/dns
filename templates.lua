344728d539ace7507b1f112387f40bb94f155f4d
Leo Adamek <leo.adamek@mrzen.com>
2017-05-25 09:32:07 +0100

    Lua != Python why did I?

--- templates.lua
+++ templates.lua
@@ -1,4 +1,4 @@
-function breaker_of_things(domain):
+function breaker_of_things(domain)
     a(domain, "139.162.242.110")
     aaaa(domain, "2a01:7e00::f03c:91ff:fed5:6a9e")
     alias(concact("www", domain), domain)
