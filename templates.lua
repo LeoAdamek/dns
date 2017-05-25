5c5072a2b74645a697973d951052ef51acdee8d5
Leo Adamek <leo.adamek@mrzen.com>
2017-05-25 09:32:45 +0100

    Learn2code u moron

--- templates.lua
+++ templates.lua
@@ -1,5 +1,5 @@
 function breaker_of_things(domain)
     a(domain, "139.162.242.110")
     aaaa(domain, "2a01:7e00::f03c:91ff:fed5:6a9e")
-    alias(concact("www", domain), domain)
+    alias(concat("www", domain), domain)
 end
