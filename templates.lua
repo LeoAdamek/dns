3091b4b278c488b34306204b857471945fa4dbe7
Leo Adamek <leo@adamek.me>
2017-05-30 15:12:12 +0100

    Updated AAAA record.

--- templates.lua
+++ templates.lua
@@ -3,6 +3,7 @@ function breaker_of_things(domain)
     aaaa(domain, "2a01:7e00::f03c:91ff:fed5:6a9e")
     alias(concat("www", domain), domain)
     a("megumi", "139.162.242.110")
+    aaaa("megumi", "2a01:7e00::f03c:91ff:fed5:6a9e")
     aaaa("megumi6", "2a01:7e00::f03c:91ff:fed5:6a9e")
     
     dkim_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGnojLHjH8AQXRmv63TlYapXXP+TS6no9Meljx8V3RqXWDwqjHTES5UgFdPk5qftHeKOK79/ZzMq5gZrrvN97FOAWb4q+tEKZzm3eKyN4A0w/vs/aLy3gr4JsV1skfrxKmXdtxDDimq7yKGunnBvm52yO2OyUHRdzuBspSU/jQ1QIDAQAB"
