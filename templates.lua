f3ed720c1f94b1a5048df5caaa1b07d4ff487ea9
Leo Adamek <leo@adamek.me>
2018-04-25 10:34:33 +0100

    Added extra records for Shoko and Kanako

--- templates.lua
+++ templates.lua
@@ -6,6 +6,10 @@ function breaker_of_things(domain)
     aaaa("megumi", "2a01:7e00::f03c:91ff:fed5:6a9e")
     aaaa("megumi6", "2a01:7e00::f03c:91ff:fed5:6a9e")
     
+    a("shoko", "81.187.27.16")
+    aaaa("shoko", "2001:8b0:92c::1")
+    aaaa("kanako", "2001:8b0:92c::2")
+    
     dkim_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGnojLHjH8AQXRmv63TlYapXXP+TS6no9Meljx8V3RqXWDwqjHTES5UgFdPk5qftHeKOK79/ZzMq5gZrrvN97FOAWb4q+tEKZzm3eKyN4A0w/vs/aLy3gr4JsV1skfrxKmXdtxDDimq7yKGunnBvm52yO2OyUHRdzuBspSU/jQ1QIDAQAB"
     
     txt("megumi._domainkey", "v=DKIM1; k=rsa; p="..dkim_key)
