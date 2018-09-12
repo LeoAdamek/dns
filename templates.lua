0d0b685b5961997f01697cb5c6b39cb79e8542ef
Leo Adamek <leo@adamek.me>
2018-09-12 18:49:40 +0100

    Update templates.lua

--- templates.lua
+++ templates.lua
@@ -1,7 +1,11 @@
 function breaker_of_things(domain)
+    
+    -- Built-in aliasing
+    alias(concat("www", domain), domain)
+    alias(concat("s", domain),domain)
+    
     a(domain, "139.162.242.110")
     aaaa(domain, "2a01:7e00::f03c:91ff:fed5:6a9e")
-    alias(concat("www", domain), domain)
     a("megumi", "139.162.242.110")
     aaaa("megumi", "2a01:7e00::f03c:91ff:fed5:6a9e")
     aaaa("megumi6", "2a01:7e00::f03c:91ff:fed5:6a9e")
@@ -9,6 +13,9 @@ function breaker_of_things(domain)
     a("shoko", "81.187.27.16")
     aaaa("shoko", "2001:8b0:92c::1")
     aaaa("kanako", "2001:8b0:92c::2")
+    aaaa("mashiro", "2001:8b0:92c::4")
+    
+    
     
     dkim_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGnojLHjH8AQXRmv63TlYapXXP+TS6no9Meljx8V3RqXWDwqjHTES5UgFdPk5qftHeKOK79/ZzMq5gZrrvN97FOAWb4q+tEKZzm3eKyN4A0w/vs/aLy3gr4JsV1skfrxKmXdtxDDimq7yKGunnBvm52yO2OyUHRdzuBspSU/jQ1QIDAQAB"
     
