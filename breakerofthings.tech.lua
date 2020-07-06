298d82da1b0070c424b5456dbbfdb76e406d3592
Leo Adamek <leo@adamek.me>
2020-07-06 19:06:13 +0000

    Add koding.breakerofthings.tech

--- breakerofthings.tech.lua
+++ breakerofthings.tech.lua
@@ -1,6 +1,9 @@
 breaker_of_things(_a)
 txt(_a, "keybase-site-verification=M4p_Qob3PBz1eECs0aTV-oJyi_dQBBhPbGLEspL3smY")
 
+aaaa(concat("koding",_a), "2a01:7e00::f03c:92ff:fe97:2943")
+alias(concat("*", "koding", _a), concat("koding", _a))
+
 alias(concat("git", _a), _a)
 alias(concat("iracing-api", _a), _a)
 cname(concat("_cfe0fe8aafd75eaeb9759a7e7ef3e639.iracing", _a), "_d8aa7efb1f57b5cd76e631726f918af6.nhqijqilxf.acm-validations.aws.")
