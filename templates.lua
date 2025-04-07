f297f756b5e4d7493559e36bcc0d1282b6c11af1
Leo Adamek <leo@adamek.me>
2025-04-07 19:27:21 +0100

    Remove unused hosts

--- templates.lua
+++ templates.lua
@@ -1,23 +1,18 @@
 function breaker_of_things(domain)
     
-    -- Built-in aliasing
+    -- Alias www. to @
     alias(concat("www", domain), domain)
-    alias(concat("s", domain),domain)
     
     a(domain, "139.162.242.110")
     aaaa(domain, "2a01:7e00::f03c:91ff:fed5:6a9e")
+
+        
     a("megumi", "139.162.242.110")
-    a("mitty", "151.236.220.70")
-    aaaa("mitty", "2a01:7e00::f03c:91ff:feaf:4e0f")
-    aaaa("megumi", "2a01:7e00::f03c:91ff:fed5:6a9e")
+    a("mitty", "151.236.220.70")    
+    aaaa("megumi", "2a01:7e00::f03c:91ff:fed5:6a9e")    
     aaaa("megumi6", "2a01:7e00::f03c:91ff:fed5:6a9e")
-    
-    a("shoko", "81.187.27.16")
-    aaaa("shoko", "2001:8b0:92c::1")
-    aaaa("kanako", "2001:8b0:92c::2")
-    aaaa("mashiro", "2001:8b0:92c::4")
-    
-    
+
+    alias(concat("s", domain), concat("megumi", domain))    
     
     dkim_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGnojLHjH8AQXRmv63TlYapXXP+TS6no9Meljx8V3RqXWDwqjHTES5UgFdPk5qftHeKOK79/ZzMq5gZrrvN97FOAWb4q+tEKZzm3eKyN4A0w/vs/aLy3gr4JsV1skfrxKmXdtxDDimq7yKGunnBvm52yO2OyUHRdzuBspSU/jQ1QIDAQAB"
     
