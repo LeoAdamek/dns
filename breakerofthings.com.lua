0666b6b5dbfa0d74f7f9ff2f7487079bfb3e60e0
Leo Adamek <leo@adamek.me>
2025-04-07 21:59:10 +0100

    Github pages alias

--- breakerofthings.com.lua
+++ breakerofthings.com.lua
@@ -2,3 +2,6 @@ breaker_of_things(_a)
 
 -- Github verification
 txt("_github-pages-challenge-LeoAdamek", "9aeeaab8cea30f9313bbd2cf681c98", 3600)
+
+-- Github pages alias
+alias(_a, "leoadamek.github.com")
