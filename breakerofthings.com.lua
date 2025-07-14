f9e3c75e1016f86292211219dfab4391ef022282
Leo Adamek <leo@adamek.me>
2025-07-14 09:53:08 +0100

    Add google site verification

--- breakerofthings.com.lua
+++ breakerofthings.com.lua
@@ -2,3 +2,4 @@ breaker_of_things(_a)
 
 -- Github verification
 txt("_github-pages-challenge-LeoAdamek", "9aeeaab8cea30f9313bbd2cf681c98", 3600)
+txt("", "google-site-verification=dMhZ6bdSrTaV2JkElS5KILDJMEPt3bdi3pe8PIsehdw", 30)
