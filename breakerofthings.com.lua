5c19add43a0cc5b77da47ca72b500b11a6c2dc29
Leo Adamek <leo@adamek.me>
2025-07-14 10:01:53 +0100

    Add DKIM

--- breakerofthings.com.lua
+++ breakerofthings.com.lua
@@ -4,3 +4,9 @@ breaker_of_things(_a)
 txt("_github-pages-challenge-LeoAdamek", "9aeeaab8cea30f9313bbd2cf681c98", 3600)
 txt("", "google-site-verification=dMhZ6bdSrTaV2JkElS5KILDJMEPt3bdi3pe8PIsehdw", 30)
 txt("", "google-site-verification=KyVJHSUI6bUi-O39iLkNb_KgvsTAa-SfjjPeo4__3XY", 30)
+
+-- Gmail DKIM
+txt("google._domainkey", 
+    "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqzfIva4K1a7uLG6TLLaVHECFwzD4zqwNXIPDbY1S2R9BI3gHTm+LVfc4tDsdtq8HQtbohPM+RRytJJExSUqXZ3rr/U+rHbCo337OgfRTJXuPlvPWnWpDJuLXECVXwpitdOPqx1HWox7dysguf7JinI+csWRZioDwNOULQzq7MVtHPIrd0E8hjFzJbD1BzwsIXg+5AfkmDdy7Mti5rRnB0+7+eQfO5kCOdhUTtvwP3Et1mmWRWb75xpLpp+jbZonu48SlZklQ02HVk4i+UeOTkA/MEl0bfqM3xuVNFWgtJv4YsjAtfbUnuK6WeBEZdivpvO540H/qFUGWZwhjWxi+XQIDAQAB",
+    3600,
+)
